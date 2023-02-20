85767914eca5d930c2ca4af496264eecf53eb0b3
Anastasiya Begunova <123498835+Egessihora@users.noreply.github.com>
2023-02-20 22:04:21 +0300

    Add file

---
+++ template_for_DECORATOR.py
@@ -0,0 +1,8 @@
+def func_decorator(func):
+    print("---  some actions ---") # будет выведен один раз в момент декорирования функции
+    def wraper(*args, **kwargs):
+        print("---  some actions ---")  # будет выполняться перед каждым вызовом функции
+        result = func(*args, **kwargs)
+        print("---  some actions ---") # будет выполняться после каждого вызова функции
+        return result
+    return wraper
